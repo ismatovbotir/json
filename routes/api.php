17f713e711d8b711e1bdea558346b9d8335d97e7
Botir Ismatov <botirismatov@MacBook-Pro-Botir.local>
2026-02-28 06:04:30 +0500

    dfadsfasdfasdfa

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::group(['prefix' => 'adines/hs'], function () {
+Route::group(['prefix' => 'adines/hs', 'middleware' => 'adines.auth'], function () {
     Route::get('/auth', [AdinesController::class, 'auth']);
     Route::resource('/product', AdinesController::class);
 });
