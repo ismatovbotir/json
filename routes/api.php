f8af01b09d8f3592c757da9944b9b5c77c69652e
Botir Ismatov <botirismatov@Botir.local>
2026-04-05 19:26:12 +0500

    dsfsdfsdfas

--- routes/api.php
+++ routes/api.php
@@ -12,3 +12,11 @@ Route::group(['prefix' => 'adines/hs', 'middleware' => 'adines.auth'], function 
     Route::get('/auth', [AdinesController::class, 'auth']);
     Route::resource('/product', AdinesController::class);
 });
+Route::group(['prefix' => 'cl-api',], function () {
+    Route::get('/{any?}', function (Request $request, ?string $any = null) {
+        return response()->json([
+            'path' => $any ?? '',
+            'parameters' => $request->query(),
+        ]);
+    })->where('any', '.*');
+});
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::any('/{any?}', function (Request $request, ?string $any = null) {
+    return response()->json([
+        'method' => $request->method(),
+        'path' => $any ?? '/',
+        'parameters' => $request->all(),
+    ]);
+})->where('any', '.*');
